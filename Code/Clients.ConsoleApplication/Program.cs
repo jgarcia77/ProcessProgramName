37fb55df7b57d7f2467c82ab472ea8e2d67cc393
jgarcia77 <jgarcia773@gmail.com>
2018-03-30 23:35:13 -0500

    Final comments

--- Code/Clients.ConsoleApplication/Program.cs
+++ Code/Clients.ConsoleApplication/Program.cs
@@ -1,15 +1,30 @@
-﻿using ApplicationCore.Services;
-using ApplicationCore.Services.Interfaces;
-using Infrastructure.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Domain = ApplicationCore.Domain.Models;
+﻿/*
+    Author:         Josue (Josh) Garcia
+    Date:           Friday, March 30, 2018
+    Description:    The function ProcessProgramName should retrieve all the program names 
+                    from the PROGRAM_NAME column in the PROGRAM table, process them, and 
+                    display them in alphabetical order. 
 
+                    When displaying the program names, an apostrophe ("'") should be 
+                    added to the beginning and the end of each program name. If there are 
+                    any apostrophes *within* a program name, then an additional apostrophe 
+                    should be added before the existing apostrophe(s). 
+
+                    Use a comma (",") to separate each program name. For example, the 
+                    string 'Test1','Test''2','Test3' is the proper output given input 
+                    values of Test1, Test'2 and Test3. 
+
+                    Please finish the function ProcessProgramName, so that it produces
+                    the correct output.
+*/
 namespace Clients.ConsoleApplication
 {
+    using ApplicationCore.Services;
+    using ApplicationCore.Services.Interfaces;
+    using Infrastructure.Repositories;
+    using System;
+    using System.Linq;
+
     class Program
     {
         static IService service;
